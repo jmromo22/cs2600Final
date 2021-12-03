f7eb8bb3066ca5c7084ad802f16797a8d8226a16
jmromo22 <jmromo@cpp.edu>
2021-12-03 06:22:02 +0000

    Global State

--- kilo.c
+++ kilo.c
@@ -13,7 +13,12 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+  struct termios orig_termios;
+};
+struct editorConfig E;
+
+
 
 /*** terminal ***/
 
@@ -26,16 +31,16 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {
 
- if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
      raw.c_cflag |= (CS8);
@@ -70,7 +75,7 @@ void editorRefreshScreen() {
    write(STDOUT_FILENO, "\x1b[H", 3);
 
     editorDrawRows();
-    
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
