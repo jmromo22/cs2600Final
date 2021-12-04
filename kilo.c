7eb71d1b0aee383881aeae94de981e23d1d150e2
jmromo22 <jmromo@cpp.edu>
2021-12-04 06:56:43 +0000

    Window size, the hard way

--- kilo.c
+++ kilo.c
@@ -15,6 +15,9 @@
 /*** data ***/
 
 struct editorConfig {
+ int screenrows;
+  int screencols;
+
   struct termios orig_termios;
 };
 struct editorConfig E;
@@ -61,10 +64,31 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+ char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+   buf[i] = '\0';
+
+ if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+
+   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+      return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
@@ -76,7 +100,7 @@ int getWindowSize(int *rows, int *cols) {
 
 void editorDrawRows() {
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.screenrows; y++) {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -106,8 +130,14 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
+
 int main() {
   enableRawMode();
+    initEditor();
 
 while (1) {
     editorRefreshScreen();
