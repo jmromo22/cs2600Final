65340b384b70964f07ca3ad1843636bfb16f4f01
jmromo22 <jmromo@cpp.edu>
2021-12-04 07:41:46 +0000

    Prevent moving the cursor

--- kilo.c
+++ kilo.c
@@ -197,16 +197,24 @@ void editorRefreshScreen() {
 void editorMoveCursor(int key) {
   switch (key) {
     case ARROW_LEFT:
+     if (E.cx != 0) {
       E.cx--;
+     }
       break;
     case ARROW_RIGHT:
+      if (E.cx != E.screencols - 1) {
       E.cx++;
+      }
       break;
     case ARROW_UP:
+      if (E.cy != 0) {
       E.cy--;
+      }
       break;
     case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1) {
       E.cy++;
+        }
       break;
   }
 }
