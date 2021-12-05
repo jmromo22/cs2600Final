89f487c19e3d236036c58f556b03345cee2dd769
jmromo22 <jmromo@cpp.edu>
2021-12-05 06:31:17 +0000

    Limiting scrolling to hte right

--- kilo.c
+++ kilo.c
@@ -280,7 +280,7 @@ void editorRefreshScreen() {
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
                                             (E.cx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
-  
+
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
@@ -291,6 +291,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
      if (E.cx != 0) {
@@ -298,9 +300,9 @@ void editorMoveCursor(int key) {
      }
       break;
     case ARROW_RIGHT:
-    
+     if (row && E.cx < row->size) {
       E.cx++;
-      
+     }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
