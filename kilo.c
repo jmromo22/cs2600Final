7acd5735d273bcdc79f72cf4f9f5aeadb6013707
jmromo22 <jmromo@cpp.edu>
2021-12-05 18:25:31 +0000

    Save to disk

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -61,7 +62,8 @@ struct editorConfig {
 };
 struct editorConfig E;
 
-
+/*** prototypes ***/
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -246,6 +248,24 @@ void editorInsertChar(int c) {
 }
 
 /*** file i/o ***/
+
+char *editorRowsToString(int *buflen) {
+  int totlen = 0;
+  int j;
+  for (j = 0; j < E.numrows; j++)
+    totlen += E.row[j].size + 1;
+  *buflen = totlen;
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+  return buf;
+}
+
 void editorOpen(char *filename) {
   free(E.filename);
   E.filename = strdup(filename);
@@ -265,6 +285,27 @@ void editorOpen(char *filename) {
   free(line);
   fclose(fp);
 }
+
+void editorSave() {
+  if (E.filename == NULL) return;
+  int len;
+  char *buf = editorRowsToString(&len);
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        editorSetStatusMessage("%d bytes written to disk", len);
+        return;
+      }
+    }
+    close(fd);
+  }
+  free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
 
 /*** append buffer ***/
 struct abuf {
@@ -458,6 +499,10 @@ void editorProcessKeypress() {
       exit(0);
       break;
 
+      case CTRL_KEY('s'):
+      editorSave();
+      break;
+
        case HOME_KEY:
       E.cx = 0;
       break;
@@ -534,7 +579,7 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-   editorSetStatusMessage("HELP: Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
 
 while (1) {
